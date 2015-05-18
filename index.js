3beaaf68e2416668faa58d74799208873fc6888f
Andrej T <andrejtrajchevski@gmail.com>
2015-05-18 02:00:54 +0200

    upload albums and save to db

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
 var multer = require('multer');
 
+var Songs = require('./lib/songsCtrl');
+
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var uploadMw = multer({
 	dest: './uploads/',
@@ -11,9 +15,30 @@ var uploadMw = multer({
 	}
 });
 
-app.post('/upload', uploadMw, function(req, res) {
-	console.log(req.files);
-	res.end('ok');
+app.post('/songs', uploadMw, function(req, res) {
+	Songs.createAll(req.files.songs.map(function(f) {
+		return {
+			name: f.originalname,
+			size: f.size,
+			artist: req.body.artist,
+			album: req.body.album,
+			path: f.name,
+		}
+	})).then(function() {
+		res.end('ok');
+	}).catch(function(e) {
+		console.log(e);
+		res.status(500).end('error')
+	});
+});
+
+app.get('/songs', function(req, res) {
+	Songs.all().then(function(songs) {
+		res.json(songs);
+	}).catch(function(e) {
+		console.log(e);
+		res.status(500).end('error')
+	});;
 });
 
 app.get('/', function(req, res) {
